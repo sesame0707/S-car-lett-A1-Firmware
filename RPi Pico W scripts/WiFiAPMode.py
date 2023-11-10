62cf706a6a472686c2c499081474c75072a837b7
sesame0707 <petergrala@gmail.com>
2023-11-10 21:38:48 +0100

    Added RPi Pico W scripts' folder

---
+++ RPi Pico W scripts/WiFiAPMode.py
@@ -0,0 +1,55 @@
+import network
+import time
+import socket
+
+
+def web_page():
+  html = """<html><head><meta name="viewport" content="width=device-width, initial-scale=1"></head>
+            <body><h1>Hello World</h1></body></html>
+         """
+  return html
+
+# if you do not see the network you may have to power cycle
+# unplug your pico w for 10 seconds and plug it in again
+def ap_mode(ssid, password):
+    """
+        Description: This is a function to activate AP mode
+        
+        Parameters:
+        
+        ssid[str]: The name of your internet connection
+        password[str]: Password for your internet connection
+        
+        Returns: Nada
+    """
+    # Just making our internet connection
+    ap = network.WLAN(network.AP_IF)
+    ap.config(essid=ssid, password=password)
+    ap.active(True)
+    # The first '192.168.8.8' is the static IP address assigned to the AP.
+    # The subnet mask is set to '255.255.255.0', defining the local network's size.
+    # The gateway IP address is '192.168.8.8', representing the device connecting the local network to the Internet.
+    # The DNS server is set to '8.8.8.8', using Google's public DNS server.
+    # Some IP addresses may not work statically, check IP address conflict or firewall settings.
+    ap.ifconfig(('192.168.4.3', '255.255.255.0', '192.168.4.3', '8.8.8.8'))
+    
+    while ap.active() == False:
+        pass
+    print('AP Mode Is Active, You can Now Connect')
+    print('IP Address To Connect to:: ' + ap.ifconfig()[0])
+    
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)   #creating socket object
+    s.bind(('', 80))
+    s.listen(5)
+
+    while True:
+      conn, addr = s.accept()
+      print('Got a connection from %s' % str(addr))
+      request = conn.recv(1024)
+      print('Content = %s' % str(request))
+      response = web_page()
+      conn.send(response)
+      conn.close()
+      
+ap_mode('S(car)lett A1',
+        '00000000')
