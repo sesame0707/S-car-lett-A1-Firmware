6c890e9f82c1fe3b1744b35a7f87380387f0afba
sesame0707 <petergrala@gmail.com>
2024-02-14 23:55:33 +0100

    WS2812B project modified
    Code matches current configuration

--- Arduino Leonardo programs/WS2812B/WS2812B.ino
+++ Arduino Leonardo programs/WS2812B/WS2812B.ino
@@ -1,7 +1,7 @@
 #include <FastLED.h>
 
 #define LED_PIN     7
-#define NUM_LEDS    60
+#define NUM_LEDS    7
 
 CRGB leds[NUM_LEDS];
 
@@ -9,7 +9,7 @@ void setup() {
   FastLED.addLeds<WS2812, LED_PIN, GRB>(leds, NUM_LEDS);
   
   for (int i = 0; i < NUM_LEDS; i++) {
-    leds[i] = CRGB(31, 31, 31);
+    leds[i] = CRGB(31, 0, 0);
   }
   FastLED.show();
 }
